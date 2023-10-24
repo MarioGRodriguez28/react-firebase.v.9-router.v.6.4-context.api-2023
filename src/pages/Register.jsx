f7a1010ae95ca040fa3a66a1c178acf4876c5035
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-10-24 18:41:53 +0200

    Login and Register,done!

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,40 @@
+import {useState} from "react";
+import {register} from "../config/firebase";
+
 const Register = () => {
-    return "Register";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("me diste submit");
+    try {
+     const credentialUser = await register({email, password});
+     console.log(credentialUser)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
